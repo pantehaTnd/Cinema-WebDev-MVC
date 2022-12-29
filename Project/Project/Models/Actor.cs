dcb73649107c43c52d0f01882c85a00d0c01abfd
pantehaTnd <84589417+pantehaTnd@users.noreply.github.com>
2022-12-29 17:23:48 +0300

    Add file via upload

---
+++ Project/Project/Models/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+        public string? ActorName { get; set; }
+        public string? Bio { get; set; }
+    }
+}
